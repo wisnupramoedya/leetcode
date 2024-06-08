6d73e572f741274f40841652486e187483758202
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2024-06-08 17:33:45 +0700

    feat: add design-circular-queue

--- README.md
+++ README.md
@@ -5,3 +5,28 @@ This is my database of Leetcode's answer as part of 1 day 1 leetcode. It might n
 Note:
 
 - Unsuccessful answer is marked with "." before the file, ensuring it stays hidden.
+
+## Step of Learning:
+I use "Competitive Programmer’s Handbook" by Antti Laaksonen as the reference.
+
+### I. Basic Techniques
+
+**Introduction**
+
+- Mathematics
+  - Basic: [^2], [^7], [^12]
+
+**Data Structures**
+
+- Queue
+  - Basic: [^622]
+
+## List of Answer:
+
+[^2]: [Add Two Numbers](./add-two-numbers.go)
+
+[^7]: [Reverse Integer](./reverse-integer.go)
+
+[^12]: [Integer to Roman](./integer-to-roman.go)
+
+[^622]: [Design Circular Queue](./design-circular-queue.go)
---
+++ design-circular-queue.go
@@ -0,0 +1,89 @@
+type MyCircularQueue struct {
+	Value []int
+	First int
+	Last  int
+	Size  int
+}
+
+func Constructor(k int) MyCircularQueue {
+	var myCircularQueue = MyCircularQueue{
+		Value: make([]int, k),
+		First: -1,
+		Last:  -1,
+		Size:  k,
+	}
+	return myCircularQueue
+}
+
+func (this *MyCircularQueue) EnQueue(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+
+	if this.First == -1 {
+		this.First = 0
+	}
+	this.Last = (this.Last + 1) % this.Size
+	this.Value[this.Last] = value
+	return true
+}
+
+func (this *MyCircularQueue) DeQueue() bool {
+	if this.IsEmpty() {
+		return false
+	}
+
+	if this.Last == this.First {
+		this.First, this.Last = -1, -1
+		return true
+	}
+
+	this.First = (this.Size + this.First + 1) % this.Size
+	return true
+}
+
+func (this *MyCircularQueue) Front() int {
+	if this.IsEmpty() {
+		return -1
+	}
+
+	return this.Value[this.First]
+}
+
+func (this *MyCircularQueue) Rear() int {
+	if this.IsEmpty() {
+		return -1
+	}
+
+	return this.Value[this.Last]
+}
+
+func (this *MyCircularQueue) IsEmpty() bool {
+	if this.First == -1 && this.Last == -1 {
+		return true
+	}
+	return false
+}
+
+func (this *MyCircularQueue) IsFull() bool {
+	if this.IsEmpty() {
+		return false
+	}
+
+	if this.First == (this.Last+1)%this.Size {
+		return true
+	}
+
+	return false
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.EnQueue(value);
+ * param_2 := obj.DeQueue();
+ * param_3 := obj.Front();
+ * param_4 := obj.Rear();
+ * param_5 := obj.IsEmpty();
+ * param_6 := obj.IsFull();
+ */
