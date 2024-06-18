fe8157c17f20de43eefc45b17a5ca69501ea276b
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2024-06-18 16:41:35 +0700

    feat: add utils for array 2D

--- utils/array.go
+++ utils/array.go
@@ -17,3 +17,24 @@ func IsArraySame[T comparable](numsA, numsB []T) bool {
 	}
 	return true
 }
+
+func Is2DArraySame[T comparable](numsA, numsB [][]T) bool {
+	if len(numsA) != len(numsB) {
+		fmt.Printf("The length of 1st level array is not the same (%d != %d)", len(numsA), len(numsB))
+		return false
+	}
+	for i := 0; i < len(numsA); i++ {
+		if len(numsA[i]) == len(numsB[i]) {
+			for j := 0; j < len(numsA[i]); j++ {
+				if numsA[i][j] != numsB[i][j] {
+					fmt.Printf("In index [%d][%d], item %v is not the same with %v", i, j, numsA[i][j], numsB[i][j])
+					return false
+				}
+			}
+		} else {
+			fmt.Printf("The length of 2nd level array in index %d is not the same (%d != %d)", i, len(numsA[i]), len(numsB[i]))
+			return false
+		}
+	}
+	return true
+}
